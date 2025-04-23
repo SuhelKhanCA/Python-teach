7a519f4f243e2457b35784c1e334a7bbc653bbe8
suhelkhanca <suhel.hata@gmail.com>
2025-04-23 22:50:08 +0530

    swapping program

---
+++ data_structures/custom_sorting.py
@@ -0,0 +1,14 @@
+lis = [23, 4, 56, 90, 1] # 1, 4, 23, 56, 90
+print(lis)
+
+for i in range(0, len(lis)):
+
+    for j in range(0, len(lis) - i - 1):
+
+        if lis[j] > lis[j+1] :
+            
+            temp = lis[j]
+            lis[j] = lis[j+1]
+            lis[j+1] = temp
+
+print(lis)            
---
+++ swap.py
@@ -0,0 +1,30 @@
+# Swapping of two numbers
+
+a = 5
+b = 6
+
+print(a)
+print(b)
+
+# temp = a 
+# a = b
+# b = temp
+
+# write a swapping logic without using temp variable : Gold
+
+# a = a + b
+# b = a - b
+# a = a - b
+
+# a = a * b
+# b = a // b
+# a = a // b
+
+a = a^b
+b = a^b
+a = a^b
+
+print(a)
+print(b)
+
+
