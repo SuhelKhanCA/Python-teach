c7ad1fe7f2216ba7c424770cc33a08e68b82eeb7
suhelkhanca <suhel.hata@gmail.com>
2025-04-25 18:43:30 +0530

    add : file handling inro

---
+++ file_handling/intro.py
@@ -0,0 +1,15 @@
+# File handling : I/P and O/P -> File
+
+f = open('name.txt', 'r')
+# f = open('c:\\Users\\suhel\\Documents\\GitHub\\Python-teach\\file_handling\\name.txt', 'r')
+
+print(type(f))
+
+names = f.read()
+print(type(names))
+print(names)
+
+names2 = f.readlines()
+print(names2)
+
+
