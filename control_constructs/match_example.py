8ee17735367b38147d39872c1422a82af72c07b9
suhelkhanca <suhel.hata@gmail.com>
2025-05-05 16:04:15 +0530

    add : match case statement

---
+++ control_constructs/match_example.py
@@ -0,0 +1,34 @@
+day = 5 # 1, 2, 3, 4,5, 6, 7
+
+# if day == 1:
+#     print("Sunday")
+# if day == 2:
+#     print("Monday")
+# if day == 3:
+#     print("Tuesday")
+# if day == 4:
+#     print("Wednesday")
+# if day == 5:
+#     print("Thursday")
+# if day == 6:
+#     print("Friday")
+# if day == 7:
+#     print("Saturday")
+
+
+match day:
+
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Moday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("Wed")
+    case 5:
+        print("Thurs")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturday")
