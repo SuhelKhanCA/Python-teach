dc7d017cdfe05863a55eca46a714bc107326bb5d
suhelkhanca <suhel.hata@gmail.com>
2025-05-05 16:37:01 +0530

    OOOPs intro

---
+++ Oops/intro.py
@@ -0,0 +1,39 @@
+# Class
+
+class Human:
+
+    # data members
+    name = None
+    height = None
+    weight = None
+    
+    # methods
+    def walk(self):
+        print(self.name, "is walking")
+
+zainab = Human()
+zainab.name = "Zainab Zamir"
+zainab.height = 160
+zainab.weight = 50
+
+print(zainab.name)
+print(zainab.height)
+print(zainab.weight)
+zainab.walk()
+
+# CW
+# suhel -> name, height, weight
+suhel = Human()
+suhel.name = "Suhel Khan"
+suhel.height = 165
+suhel.weight = 62
+
+print(suhel.name)
+print(suhel.height)
+print(suhel.weight)
+suhel.walk()
+
+
+# HW
+
+# Car -> brand, model, speed : 2 objects
