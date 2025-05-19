ea0e89c50eb7ca783dfb611d0ae3217929f3c219
suhelkhanca <suhel.hata@gmail.com>
2025-05-19 11:53:21 +0530

    add : practice question

---
+++ practice/day1/Q1.py
@@ -0,0 +1,16 @@
+# Calculate area and perimeter of a circle
+
+# Calculate area and perimeter of a rectangle
+
+# 2 * pie * r
+# pie * r * r
+
+rad = int(input("Enter the radius of circle: "))
+
+area = 3.14 * rad * rad
+
+perimeter = 2 * 3.14 * rad
+
+
+print("Area:", area)
+print("Perimeter:", perimeter)
